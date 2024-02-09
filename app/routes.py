ac876d2e1527cc3cfb739cccead4f2e754dc92a3
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2024-02-09 22:55:10 +0530

    do python3 main.py to run the website on localhost

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,3 @@ from app import app
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/add',method=['POST','GET'])
-def add():
-    if request.method == 'POST':
-        with open('login.txt','a+') as f:
-            f.write()
---
+++ main.py
@@ -0,0 +1,4 @@
+from app import app
+from app import routes
+
+app.run(debug=True)
